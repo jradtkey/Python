6af4cbeb14f5f6a75f239763b2f3dc2d7f7de0c9
Jared <jradtkey@gmail.com>
2018-01-07 11:17:06 -0800

    stack

---
+++ stack.py
@@ -0,0 +1,31 @@
+#LAST IN FIRST OUT
+
+class Stack(object):
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        print self.items == []
+        return self.items == []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        print self.items.pop()
+        self.items.pop()
+
+    def peek(self):
+        print self.items[len(self.items)-1]
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        print len(self.items)
+        return len(self.items)
+
+
+s = Stack()
+s.push("hello")
+s.push("world")
+s.pop()
