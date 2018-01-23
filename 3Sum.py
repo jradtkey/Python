109e319e2773c51d38a49ba50851c4c7b057c22f
Jared <jradtkey@gmail.com>
2018-01-22 20:17:52 -0800

    3sum

---
+++ 3Sum.py
@@ -0,0 +1,24 @@
+def threeSum(arr):
+
+    newArr = []
+    new = []
+    for i in range(len(arr)):
+        j = 0
+        b = j+1
+        if i == j:
+            continue
+        if i == b:
+            continue
+        while b < len(arr):
+            if arr[i] + arr[j] + arr[b] == 0:
+                new.append(arr[i])
+                new.append(arr[j])
+                new.append(arr[b])
+                new.sort()
+                if new not in newArr:
+                    newArr.append(new)
+                new = []
+            b = b + 1
+    print newArr
+arr = [3,0,-2,-1,1,2]
+threeSum(arr)
